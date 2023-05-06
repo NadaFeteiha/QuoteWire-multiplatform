a0ab9b48341ee444f313c7aa8f02375be932890e
Nada Feteiha <nada.feteiha@gmail.com>
2023-05-06 02:31:13 -0700

    add lottie

---
+++ androidApp/src/androidMain/kotlin/com/myapplication/ui/compose/ErrorLottieAnimations.kt
@@ -0,0 +1,33 @@
+package com.myapplication.ui.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.myapplication.R
+import ui.modifiers.nonRippleEffect
+
+@Composable
+fun ErrorLottieAnimations(
+    repeatCount: Int = 100,
+    onClickTryAgain: () -> Unit
+) {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.error))
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        LottieAnimation(composition = composition, iterations = repeatCount)
+
+        QuoteButton(modifier = Modifier.nonRippleEffect(onClickTryAgain), text = "Try again!")
+    }
+}
---
+++ androidApp/src/androidMain/kotlin/com/myapplication/ui/compose/LottieAnimation.kt
@@ -0,0 +1,34 @@
+package com.myapplication.ui.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.LottieDrawable
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import ui.modifiers.nonRippleEffect
+
+@Composable
+fun LottieAnimations(
+    lottieRes: Int,
+    repeatCount: Int = 100
+) {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(lottieRes))
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LottieAnimation(
+            composition = composition,
+            iterations = repeatCount
+        )
+    }
+}
+
